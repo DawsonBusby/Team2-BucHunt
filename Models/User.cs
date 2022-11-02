8eb0cd2b7f9d3ad4ee87e553b8f9feb74f477f46
Zachary Hower <54638686+Zathoso@users.noreply.github.com>
2022-11-02 14:30:44 -0400

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,34 @@
+﻿namespace ETSUBucHunt.WebApp.Models
+{
+    public class User
+    {
+        //The User's email and phone numbers will be stored in these strings.
+        public String UserEmailAddress { get; set; }
+        public String UserPhoneNumber { get; set; }
+
+        //default constructor with default values
+        public User()
+        {
+            UserEmailAddress = "default ";
+            UserPhoneNumber = "default";
+        }
+
+
+        //Normal constructor that the web page can call to pass through data that will be stored as new user.
+        public User(String inputEmailAddress, String inputPhoneNumber)
+        {
+            UserEmailAddress = inputEmailAddress;
+            UserPhoneNumber = inputPhoneNumber;
+        }
+
+        //Testing the new class object.
+        /*
+             static void Main(string[] args)
+            {
+                User A = new User("test@test.edu", "123-456-7890");
+                Console.WriteLine(A.UserEmailAddress + " " + A.UserPhoneNumber);
+
+            }
+        */
+    }
+}
