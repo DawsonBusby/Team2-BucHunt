74574d14542e0cb062bdf6bc9541285d125348f2
Zachary Hower <54638686+Zathoso@users.noreply.github.com>
2022-11-07 14:26:29 -0500

    Add files via upload

--- Models/User.cs
+++ Models/User.cs
@@ -5,18 +5,21 @@
         //The User's email and phone numbers will be stored in these strings.
         public String UserEmailAddress { get; set; }
         public String UserPhoneNumber { get; set; }
+        public String UserName { get; set; }
 
         //default constructor with default values
         public User()
         {
+            UserName = "default";
             UserEmailAddress = "default ";
             UserPhoneNumber = "default";
         }
 
 
         //Normal constructor that the web page can call to pass through data that will be stored as new user.
-        public User(String inputEmailAddress, String inputPhoneNumber)
+        public User(String inputUserName, String inputEmailAddress, String inputPhoneNumber)
         {
+            UserName = inputUserName;   
             UserEmailAddress = inputEmailAddress;
             UserPhoneNumber = inputPhoneNumber;
         }
@@ -25,8 +28,8 @@
         /*
              static void Main(string[] args)
             {
-                User A = new User("test@test.edu", "123-456-7890");
-                Console.WriteLine(A.UserEmailAddress + " " + A.UserPhoneNumber);
+                User A = new User("testname", "test@test.edu", "123-456-7890");
+                Console.WriteLine(A.UserName + " " + A.UserEmailAddress + " " + A.UserPhoneNumber);
 
             }
         */
