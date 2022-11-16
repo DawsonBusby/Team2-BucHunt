c7655d26b1b3caee29429d071b5580a2cb5d270b
Oluwatofunmi Olubiyi <OLUBIYI@ETSU.EDU>
2022-11-15 21:59:44 -0500

    created method to create task

---
+++ ETSUBucHunt.WebAPI/Models/TaskDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ETSUBucHunt.WebAPI.Models
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext(DbContextOptions<TaskDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Tasks> Tasks { get; set; } = null!;
+    }
+}
--- ETSUBucHunt.WebAPI/Program.cs
+++ ETSUBucHunt.WebAPI/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ETSUBucHunt.WebAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TaskDbContext>(opt =>
+    opt.UseInMemoryDatabase("Task"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
