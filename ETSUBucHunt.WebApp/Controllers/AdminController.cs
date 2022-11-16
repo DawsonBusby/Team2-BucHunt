5c7b89d5f5eb641a7c20ae2cd18fb9ce5d95d95a
mburly <mburleson@cfl.rr.com>
2022-11-16 13:49:31 -0500

    admin view

---
+++ ETSUBucHunt.WebApp/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ETSUBucHunt.WebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ETSUBucHunt.WebApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace ETSUBucHunt.WebApp.Models
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+        public int admin { get; set; }
+
+
+        public User()
+        {
+
+        }
+    }
+}
